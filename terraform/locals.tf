07db39b4587786d2ff66d7a9d838e3ce9f8da57c
Alex Harvey <alexharv074@gmail.com>
2026-03-23 12:14:38 +1100

    Update laptop IP

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -3,7 +3,7 @@ locals {
   aws_region              = "ap-southeast-2"
   db_username             = "wikirdb"
   github_repo             = "alex-harvey-z3q/wiki-rag-agentic"
-  laptop_ip               = "124.168.117.62/32"
+  laptop_ip               = "125.63.140.154/32"
   bedrock_chat_model_id   = "anthropic.claude-3-5-sonnet-20241022-v2:0"
   bedrock_embed_model_id  = "amazon.titan-embed-text-v2:0"
   embed_dim               = "1024"
